79dca9cf9f73f2c4eb5b3a437fcfb4420588c99d
Clifford-OA <cliffordosei222@gmail.com>
2022-06-07 15:16:04 +0000

    Orders and Employees page UI

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {
+  GridComponent, ColumnsDirective, ColumnDirective,
+  Page, Inject, Search, Toolbar
+} from '@syncfusion/ej2-react-grids';
 
-export const Employees = () => {
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
+
+const Employees = () => {
   return (
-    <div>Employee</div>
+    <div className='m-2 md:m-2 p-2 md:p-10 bg-white rounded-xl'>
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging={true}
+        allowSorting={true}
+        toolbar={['Search']}
+        width="auto" >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
-}
+};
 
 export default Employees;
